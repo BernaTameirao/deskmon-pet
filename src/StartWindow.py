a8d01a965bcbd7e5d3d9a6177a9fae3e8bfbdbf4
Bernardo Tameirão <bernardotameirao@gmail.com>
2026-06-06 19:53:04 -0300

    Feat: display sprites in main window

--- src/StartWindow.py
+++ src/StartWindow.py
@@ -1,7 +1,9 @@
 import os
 from functools import partial
-from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QPushButton, QApplication, QWidget
-from PyQt5.QtCore import Qt
+from PIL import Image
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QPushButton, QApplication, QWidget, QLabel, QScrollArea
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtGui import QIcon, QPixmap
 
 from Pet import Pet
 from PetManager import PetManager
@@ -18,6 +20,7 @@ class StartWindow(QMainWindow):
         # Window configurations
         self.setWindowTitle("Deskmon Pet")
         self.setFixedWidth(300)
+        self.setFixedHeight(300)
         self.setStyleSheet(self._info_style())
         self.setWindowFlags(
             Qt.WindowType.Window |
@@ -29,20 +32,34 @@ class StartWindow(QMainWindow):
         self._build_ui()
     
     def _build_ui(self):
+        scroll = QScrollArea()
+        scroll.setWidgetResizable(True)
+        self.setCentralWidget(scroll)
+
         central_widget = QWidget()
-        self.setCentralWidget(central_widget)
+        scroll.setWidget(central_widget)
 
         layout = QVBoxLayout()
         central_widget.setLayout(layout)
 
+        label = QLabel()
+        label.setFixedHeight(50)
+        label.setText("Your Deskmon:")
+        layout.addWidget(label)
+
         for name in self.pet_names:
-            button = QPushButton(name.capitalize())
+            img = Image.open(f"imgs/{name}_0.png")
+            x1, y1, x2, y2 = img.getbbox()
+
+            pixmap = QPixmap(f"imgs/{name}_0.png")
+            pixmap = pixmap.copy(x1, y1, x2-x1, y2-y1)
+
+            button = QPushButton()
+            button.setIcon(QIcon(pixmap))
+            button.setIconSize(QSize(64, 64))
             button.clicked.connect(partial(self.create_pet, name))
 
             layout.addWidget(button)
-        
-        self.adjustSize()
-        self.setMaximumHeight(self.sizeHint().height())
 
     def _info_style(self):
         base_dir = os.path.dirname(os.path.abspath(__file__))
